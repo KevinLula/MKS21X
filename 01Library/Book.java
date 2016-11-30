0bc2fd8cd4a71f5b531e1ff1d61abc968d03e8c6
Kevin <kevinlula2014@gmail.com>
2016-11-30 03:16:45 -0500

    Updated 11/30/16

--- 01Library/Book.java
+++ 01Library/Book.java
@@ -1,6 +1,44 @@
 public class Book{
+	private String author;
+	private String title;
+	private String ISBN;
+
+    public Book(){
+    }
+
+    public Book(String Author, String Title, String isbn){
+	author = Author;
+	title = Title;
+	ISBN = isbn;
+    }
+
+    public String getAuthor(){
+	return author;
+    }
+
+    public String getTitle(){
+	return title;
+    }
+
+    public String getISBN(){
+	return ISBN;
+    }
+
+    public void setAuthor(String Author){
+	author = Author;
+    }
+
+    public void setTitle(String Title){
+	title = Title;
+    }
+    
+    public void setISBN(String isbn){
+	ISBN = isbn;
+    }
+
+    public String toString(){
+	return "";
+    }
+	
 
 
-
-
-}
