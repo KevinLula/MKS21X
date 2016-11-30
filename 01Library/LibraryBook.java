c4d45ded026e67745018d568745d3de176f25135
Kevin <kevinlula2014@gmail.com>
2016-11-30 03:26:30 -0500

    New 11/30/16

---
+++ 01Library/LibraryBook.java
@@ -0,0 +1,26 @@
+abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+
+    public LibraryBook(String a, String t, String i, String c){
+	super(a, t, i);
+	callNumber = c;
+    }
+
+    public String getCallNumber(){
+	return callNumber;
+    }
+
+    public void setCallNumber(String c){
+	callNumber = c;
+    }
+
+    abstract void checkout(String patron, String due);
+
+    abstract void returned();
+
+    abstract String circulationStatus();
+
+    
+
+    public String toString(){
+    }
