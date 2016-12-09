edef781600ea7c02173343af3b66db67ecdc7889
Kevin <kevinlula2014@gmail.com>
2016-12-09 03:30:16 -0500

    New checkSum, get_Zip, toCode

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -19,20 +19,27 @@ public class Barcode implements Comparable<Barcode>{
 	_checkDigit = temp % 10;}
 	}
 	
-	
-// postcondition: Creates a copy of a bar code.
-  public Barcode clone(){}
+
 
 
 // postcondition: computes and returns the check sum for _zip
-  private int checkSum(){
-	return (int)_zip + _checkDigit;}
+  private static int checkSum(String z){
+	int x = 0
+        for(int y = 0; y < z.length(); y++){
+	x = x + z.get(y);
+	}
+	return (int)z + (x % 10)}
+
+	public String get_Zip(){
+	return _zip;
+}
+
 
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
   public String toString(){
 	String temp = _zip + _checkDigit
-	String ans = ""
+	String ans = "|"
 	for(int x = 0; x < temp.length(); x++){
 	switch(temp.charAt(x)){
 	case 0: ans = ans + "||:::";
@@ -58,15 +65,52 @@ public class Barcode implements Comparable<Barcode>{
 	default: ans = ans + "";
 		break;}
 		}
-	System.out.println(ans);
+	return temp + "" + ans + "|";
 	}
 
 
 // postcondition: compares the zip + checkdigit, in numerical order. 
   public int compareTo(Barcode other){
+	return checkSum(_zip).compareTo(checkSum(other.get_Zip()));
 	}
     
 
 //Hint: a switch statement is your friend here. It is optional, however.
+
+	public String toCode(String zip){
+	String temp = checkSum(zip)
+        String ans = "|"
+	for(int x = 0; x < temp.length(); x++){
+	switch(temp.charAt(x)){
+	case 0: ans = ans + "||:::";
+		break;
+	case 1: ans = ans + ":::||";
+		break;
+	case 2: ans = ans + "::|:|";
+		break;
+	case 3: ans = ans + "::||:";
+		break;
+	case 4: ans = ans + ":|::|";
+		break;
+	case 5: ans = ans + ":|:|:";
+		break;
+	case 6: ans = ans + ":||::";
+		break;
+	case 7: ans = ans + "|:::|";
+		break;
+	case 8: ans = ans + "|::|:";
+		break;
+	case 9: ans = ans + "|:|::";
+		break; 
+	default: ans = ans + "";
+		break;}
+		}
+	return ans + "|";
+	}
+}
+
+	public int toZip(String code){
+	
+}
 
 
