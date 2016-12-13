5d1166efd8d42a1a6d659fa2f44ab7ea8d82b85e
KevinLula <kevinlula2014@gmail.com>
2016-12-13 18:56:27 -0500

    doesnt work with oddnumbered data.length

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -5,30 +5,36 @@ public class Sorts{
     }
 
     public static void selectionSort(int [] data){
-	int temp = 0;
+	int position = 0;
+	int value = 0;
 	for(int x = 0; x < data.length; x++){
 	    for(int a = x; a < data.length; a++){
 		if(data[a] <= data[x]){
-		    temp = data[a];
-		}
-		    data[x] = temp;
+		    position = a;
+		    value = data[a];
+		}		
 	    }
+	    data[position] = data[x];
+	    data[x] = value;
 	}
     }
 
     public static String toString(int[]data){
 	String str = "{";
 	    for(int x = 0; x < data.length; x++){
+		if(x + 1 == data.length){
+		    str = str + data[x] + "}";
+		    break;
+			}
 		str = str + data[x] + ", ";
 	    }
-	str = str + "}";
 	return str;
     }
 
 
 public static void main(String [] args){
     System.out.println(name());
-    int[]data = {5, 9, 4, 3, 18};
+    int[]data = {5, 9, 3, 18};
     selectionSort(data);
     System.out.println(toString(data));
 }
