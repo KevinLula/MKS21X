2f2bceea6c6242a9b50640dd71ef36204e97e2ca
Kevin <kevinlula2014@gmail.com>
2016-12-14 03:32:04 -0500

    Finished SelectionSort started InsertionSort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -6,18 +6,29 @@ public class Sorts{
 
     public static void selectionSort(int [] data){
 	int position = 0;
-	int value = 0;
+	int temp = 0;
 	for(int x = 0; x < data.length; x++){
+		temp = data[x];
 	    for(int a = x; a < data.length; a++){
-		if(data[a] <= data[x]){
+		if(data[a] <= temp){
 		    position = a;
-		    value = data[a];
+		    temp = data[a];
 		}		
 	    }
 	    data[position] = data[x];
-	    data[x] = value;
+	    data[x] = temp;
+	    temp = 0;
 	}
     }
+		
+			
+	public insertionSort(int[]data){
+	int position = 0;
+	int value = 0;
+	for(int x = 1; x < data.length; x++){
+		for(int y = x; y > 0; y--){
+			if(data[y] < data[y-1])
+				
 
     public static String toString(int[]data){
 	String str = "{";
@@ -34,7 +45,7 @@ public class Sorts{
 
 public static void main(String [] args){
     System.out.println(name());
-    int[]data = {5, 9, 3, 18};
+    int[]data = {5, 9, 3, 7, 12, 10, 18, 1, 0};
     selectionSort(data);
     System.out.println(toString(data));
 }
