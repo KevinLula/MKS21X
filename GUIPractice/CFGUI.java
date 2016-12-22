9d437a4b83dce09bad0a53c7a957ee190cf8bd7e
Kevin <kevinlula2014@gmail.com>
2016-12-22 03:20:40 -0500

    New Layout

--- GUIPractice/CFGUI.java
+++ GUIPractice/CFGUI.java
@@ -5,17 +5,17 @@ import java.awt.event.*;
 public class CFGUI extends JFrame implements ActionListener{
     private Container pane;
     private JLabel j;
-    private JTextField t;
+    private JTextField t, t2;
     private JButton b, b2;
 
    public CFGUI() {
     this.setTitle("My first GUI");
-    this.setSize(600,400);
+    this.setSize(400,400);
     this.setLocation(100,100);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     pane = this.getContentPane();
-    pane.setLayout(new FlowLayout());
+    pane.setLayout(new BorderLayout());
       JButton b = new JButton("Convert to Farenheit");
       b.addActionListener(this);
       b.setActionCommand("CtoF");
@@ -23,20 +23,20 @@ public class CFGUI extends JFrame implements ActionListener{
       b2.addActionListener(this);
       b2.setActionCommand("FtoC");
      t = new JTextField(10);
-     j = new JLabel("Temperature Converter");
-     pane.add(t);
-     pane.add(b);
-     pane.add(b2);
-     pane.add(j);
+     t2 = new JTextField(10);
+     pane.add(t, BorderLayout.PAGE_START);
+     pane.add(b, BorderLayout.LINE_START);
+     pane.add(b2, BorderLayout.LINE_END);
+     pane.add(t2, BorderLayout.PAGE_END);
   }
 
   public void actionPerformed(ActionEvent e){
    String event = e.getActionCommand();
    if(event.equals("CtoF")){
-       t.setText(Double.toString(CtoF(Double.parseDouble(t.getText()))) + " F");
+       t2.setText(Double.toString(CtoF(Double.parseDouble(t.getText()))) + " F");
    }
    if(event.equals("FtoC")){
-       t.setText(Double.toString(FtoC(Double.parseDouble(t.getText()))) + " C");
+       t2.setText(Double.toString(FtoC(Double.parseDouble(t.getText()))) + " C");
    }
   }
 
